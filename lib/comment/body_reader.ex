600d4dd70482fd3c659762ba1e9af4c67c80cb02
Mijail Rondon <mijail@riseup.net>
2019-11-21 05:28:50 -0500

    Add documentation to BodyReader

--- lib/comment/body_reader.ex
+++ lib/comment/body_reader.ex
@@ -1,6 +1,13 @@
 defmodule Comment.BodyReader do
-  def read_body(conn, opts) do
-    {:ok, body, conn} = Plug.Conn.read_body(conn, opts)
+  @moduledoc """
+  Used for store a raw copy of the request body before be
+  processed by Plug.Parsers to be used in the signature verification.
+  """
+  alias Plug.Conn
+
+  @spec read_body(Conn.t(), Keyword.t()) :: {:ok, binary, Conn.t()} | {:error, Conn.term()}
+  def read_body(%Conn{} = conn, opts) do
+    {:ok, body, conn} = Conn.read_body(conn, opts)
     conn = update_in(conn.assigns[:raw_body], &[body | &1 || []])
     {:ok, body, conn}
   end
