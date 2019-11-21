5d6ec844e687487c73ba71346bf040fadfb9ebde
Mijail Rondon <mijail@riseup.net>
2019-11-21 00:42:11 -0500

    Remove unnecesary logic

--- test/comment/github_test.exs
+++ test/comment/github_test.exs
@@ -1,33 +1,3 @@
 defmodule Comment.GithubTest do
   use ExUnit.Case
-
-  alias Comment.Github
-
-  doctest Github
-
-  setup_all do
-    Application.put_env(:comment, :github, [
-      {:secret_key, "123"}
-    ])
-  end
-
-  test "check good signature" do
-    payload =
-      Github.verify_signature(
-        "hello world!",
-        "sha1=a4e4e63faa4741c471b5e4fc0284feb41c085ac7"
-      )
-
-    assert payload == {:ok, "hello world!"}
-  end
-
-  test "check bad signature" do
-    payload =
-      Github.verify_signature(
-        "hello world!",
-        "sha256=bee0f432b504b5f2bd795554e95606a17fee40b07f108463aeabc8d169b1a7f1"
-      )
-
-    assert {:error, "not the same digest"} == payload
-  end
 end
