1c82f3fd1169899d794d883854e8946a953a6e41
Mijail Rondon <mijail@riseup.net>
2019-11-20 21:04:34 -0500

    Fix env vars

--- config/dev.exs
+++ config/dev.exs
@@ -2,6 +2,6 @@ import Config
 
 config :comment, Comment.Repo,
   database: "comment_dev",
-  hostname: System.get_env("DATABASE_HOSTNAME", "localhost"),
-  username: System.get_env("DATABASE_USERNAME", "postgres"),
-  password: System.get_env("DATABASE_PASSWORD", "")
+  hostname: System.get_env("POSTGRES_HOST", "localhost"),
+  username: System.get_env("POSTGRES_USER", "postgres"),
+  password: System.get_env("POSTGRES_PASSWORD", "")
