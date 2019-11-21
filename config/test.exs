24663ac066531cf76f73449bd24445fd152785df
Mijail Rondon <mijail@riseup.net>
2019-11-20 20:52:21 -0500

    Fix postgresql actions

--- config/test.exs
+++ config/test.exs
@@ -1,6 +1,8 @@
-import Mix.Config
+import Config
 
 config :comment, Comment.Repo,
   database: "comment_test",
-  hostname: "localhost",
+  hostname: System.get_env("DATABASE_HOSTNAME", "localhost"),
+  username: System.get_env("DATABASE_USERNAME", "postgres"),
+  password: System.get_env("DATABASE_PASSWORD", ""),
   pool: Ecto.Adapters.SQL.Sandbox
