54f95de9185595306d624c55e8463e43cae7e0dc
Mijail Rondon <mijail@riseup.net>
2019-11-20 20:08:15 -0500

    Add github checks

--- lib/comment/github.ex
+++ lib/comment/github.ex
@@ -4,17 +4,32 @@ defmodule Comment.Github do
   and pull requests.
   """
   alias Comment.Crypto
+  alias Comment.Github.{Tokens}
 
   def handle_payload(payload, signature) do
     payload
     |> verify_signature(signature)
+    |> json_payload()
+    |> authenticate_instalation()
   end
 
-  defp verify_signature(payload, signature) do
-    [_type, digest] = String.split(signature, "=")
-    key = github_config()[:secret_key]
+  @doc """
+  Verify the signature of a payload
 
-    result_digest = Crypto.calculate_signature(key, payload)
+  # Example
+  iex> Comment.Github.verify_signature("random payload", "sha1=6f4aeff3af1d7c052a99a47917f478c5b88ed5c5")
+  {:ok, "random payload"}
+  iex> Comment.Github.verify_signature("bad signature", "sha1=55b037a2f2380af2a4283a9cc1b245b74854eab5")
+  {:error, "not the same digest"}
+  """
+
+  @spec verify_signature(binary, binary) :: {:ok, binary} | {:error, binary}
+  def verify_signature(payload, signature) do
+    [_type, digest] = String.split(signature, "=")
+
+    result_digest =
+      secret_key()
+      |> Crypto.calculate_signature(payload)
 
     case result_digest do
       ^digest ->
@@ -25,7 +40,22 @@ defmodule Comment.Github do
     end
   end
 
-  defp github_config do
-    Application.fetch_env!(:comment, :github)
+  defp json_payload({:ok, payload}) do
+    Jason.decode!(payload)
+  end
+
+  defp authenticate_instalation(payload) do
+    instalation_id = payload["installation"]["id"]
+    token = Tokens.generate_and_sign!()
+
+    client =
+      Tentacat.Client.new(%{jwt: token})
+      |> Tentacat.App.Installations.token(instalation_id)
+
+    {:ok, client, payload}
+  end
+
+  defp secret_key do
+    Application.fetch_env!(:comment, :github_secret_key)
   end
 end
