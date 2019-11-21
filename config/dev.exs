bcb29d080ab9ab76290cdb26ab80c130a55eb23f
Mijail Rondon <mijail@riseup.net>
2019-11-20 20:52:21 -0500

    Fix postgresql actions

--- config/dev.exs
+++ config/dev.exs
@@ -1,5 +1,7 @@
-import Mix.Config
+import Config
 
 config :comment, Comment.Repo,
   database: "comment_dev",
-  hostname: "localhost"
+  hostname: System.get_env("DATABASE_HOSTNAME", "localhost"),
+  username: System.get_env("DATABASE_USERNAME", "postgres"),
+  password: System.get_env("DATABASE_PASSWORD", "")
