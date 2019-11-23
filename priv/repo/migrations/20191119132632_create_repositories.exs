f7ae6aaadca5fdcbe7684de0a1b95928d065f1b7
Mijail Rondon <mijail@riseup.net>
2019-11-23 04:04:11 -0500

    Fix migrations

--- priv/repo/migrations/20191119132632_create_repositories.exs
+++ priv/repo/migrations/20191119132632_create_repositories.exs
@@ -3,7 +3,7 @@ defmodule Comment.Repo.Migrations.CreateRepositories do
 
   def change do
     create table(:repositories) do
-      add(:repository_id, :integer, null: false)
+      add(:repository, :integer, null: false)
       add(:name, :string, null: false)
       add(:full_name, :string, null: false)
       add(:private, :boolean, default: false)
@@ -11,5 +11,7 @@ defmodule Comment.Repo.Migrations.CreateRepositories do
 
       timestamps()
     end
+
+    create(unique_index(:repositories, [:repository]))
   end
 end
